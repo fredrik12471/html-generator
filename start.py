af2ef4f61df03099af06c025c1a51613a71fa28a
Fredrik W <fredrik12471@gmail.com>
2019-09-30 22:53:06 +0200

    [task] Added links to generated pages

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ def create(file, date):
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>It is me again</title>
+        <title>Tapas Theory</title>
         <!-- meta name="viewport" content="width=device-width, initial-scale=1" -->
         <!--link rel="stylesheet" type="text/css" media="screen" href="main.css" / -->
         <script src="main.js"></script>
@@ -15,13 +15,34 @@ def create(file, date):
     <body>
     <h1>Hello World</h1>
     <h2>Updated """ + date + """</h2>
+    <p><a href="twitter/fwsthlm.html">The Twitter Tapas</a> </p>
     </body>
     </html>"""
     f.write(text)
 
-
+def createTwitterPage(file):
+    f = open(file, 'w')
+    text = """<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Tapas Theory</title>
+        <!-- meta name="viewport" content="width=device-width, initial-scale=1" -->
+        <!--link rel="stylesheet" type="text/css" media="screen" href="main.css" / -->
+        <script src="main.js"></script>
+    </head>
+    <body>
+    <h1>Twitter Tapas</h1>
+    <h2>The twitter tapas for fwsthlm</h2>
+    <p><a href="../index.html">Home</a> </p>
+    </body>
+    </html>"""
+    f.write(text)
 
 now = datetime.now() # current date and time
 today = now.strftime("%Y%m%d-%H:%M:%S")
 #create("C:\\Users\\Fredrik\\git\\http-sandbox\\index.html", today)
 create("/data/data/com.termux/files/home/git/http-sandbox/index.html", today)
+#createTwitterPage("C:\\Users\\Fredrik\\git\\http-sandbox\\twitter\\fwsthlm.html")
+createTwitterPage("/data/data/com.termux/files/home/git/http-sandbox/twitter/fwsthlm.html")
--- update-http.sh
+++ update-http.sh
@@ -1,11 +1,13 @@
 cd ../../git/http-sandbox/
+#cd ..\\..\\git\\http-sandbox\\
 git pull
 git checkout -b newbranch
 DATE=$(date +"%Y%m%d%H%M")
 #echo 'Updated ' $DATE >> /data/data/com.termux/files/home/git/http-sandbox/index.html
 #sed -i 's/<h2>Updated [0-9]*/<h2>Updated '$DATE'/g' /data/data/com.termux/files/home/git/http-sandbox/index.html
 python /data/data/com.termux/files/home/git/html-generator/start.py
-git add index.html
+#python C:\\Users\\Fredrik\\git\\html-generator\\start.py
+git add --all
 git commit -m 'Update' -m "'$DATE'"
 git push origin newbranch:master
 git checkout master
