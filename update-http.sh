ffb271772dbd2de9582292fc5c848ea1221a4b9f
Fredrik W <fredrik12471@gmail.com>
2019-11-13 21:40:08 +0100

    [task] Commit to clean large files

--- update-http.sh
+++ update-http.sh
@@ -6,6 +6,7 @@ DATE=$(date +"%Y%m%d%H%M")
 #echo 'Updated ' $DATE >> /data/data/com.termux/files/home/git/http-sandbox/index.html
 #sed -i 's/<h2>Updated [0-9]*/<h2>Updated '$DATE'/g' /data/data/com.termux/files/home/git/http-sandbox/index.html
 python /data/data/com.termux/files/home/git/html-generator/start.py
+echo "$(tail -50 /data/data/com.termux/files/home/git/http-sandbox/twitter/aikfotboll/unfollowers.txt)" > /data/data/com.termux/files/home/git/http-sandbox/twitter/aikfotboll/unfollowers.txt
 #python C:\\Users\\Fredrik\\git\\html-generator\\start.py
 git add --all
 git commit -m 'Update' -m "'$DATE'"
